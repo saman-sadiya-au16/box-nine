49aff03babfa80ccbe5410f7b297ed1efc651517
saman-sadiya-au9 <samansadiya@gmail.com>
2021-03-12 18:29:19 +0530

    heroku deploy part two

--- routes/auth.js
+++ routes/auth.js
@@ -23,11 +23,11 @@ router.post(
 );
 
 router.get('/login', function(req, res, next) {
-    res.render('login', { title: 'Login' });
+    res.render('login', { title: 'Login', script: 'adminlogin.js' });
 });
 
 router.get('/signup', function(req, res, next) {
-    res.render('signup', { title: 'Signup' });
+    res.render('signup', { title: 'Signup', script:  'signup.js'});
 });
 
 router.get("/signout", signout);
